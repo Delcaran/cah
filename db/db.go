7f8fd3854fee27556b18e1f26da12444da9ab880
PAOLUZZIM <matteo.paoluzzi@leonardo.com>
2024-01-24 16:09:57 +0100

    form parsing

--- db/db.go
+++ db/db.go
@@ -11,14 +11,36 @@ var db_eng string
 //go:embed ita.json
 var db_ita string
 
+type Card interface {
+	Print() string
+	getPick() uint
+}
+
 type BlackCard struct {
 	Text string `json:"text"`
 	Pick uint   `json:"pick"`
 }
 
+func (c BlackCard) Print() string {
+	return c.Text
+}
+func (c BlackCard) getPick() uint {
+	return c.Pick
+}
+
+
 type WhiteCard struct {
 	Text string
 }
+
+func (c WhiteCard) Print() string {
+	return c.Text
+}
+func (c WhiteCard) getPick() uint {
+	return 0
+}
+
+var database Database
 
 type Set struct {
 	Name string `json:"name"`
@@ -34,19 +56,60 @@ type Database struct {
 }
 
 type SelectedCards struct {
-	black []BlackCard
-	white []WhiteCard
+	black []Card
+	white []Card
 }
 
-func Load(lang string) (*Database, error){
+func Load(lang string) (*Database, error) {
 	var values string
-	data := Database{}
 	switch lang {
 	case "ita":
 		values = db_ita
 	case "eng":
 		values = db_eng
 	}
-    json.Unmarshal([]byte(values), &data)
-	return &data, nil
+    json.Unmarshal([]byte(values), &database)
+	return &database, nil
+}
+
+func cleanup_card_list(cards []uint) []uint {
+	var cleaned []uint
+	for _, x := range cards {
+		present := false
+		for _, y := range cleaned {
+			if x == y {
+				present = true
+				break
+			}
+		}
+		if !present {
+			cleaned = append(cleaned, x)
+		}
+	}
+	return cleaned
+}
+
+func GetSelectedCards(sets []int) (SelectedCards, error) {
+	var wanted_blacks []uint
+	var wanted_whites []uint
+	for index, set := range database.Sets {
+		for _, sel := range sets {
+			if sel == index {
+				wanted_blacks = append(wanted_blacks, set.BlackIDs...)
+				wanted_whites = append(wanted_whites, set.WhiteIDs...)
+			}
+		}
+	}
+	wanted_blacks = cleanup_card_list(wanted_blacks)
+	wanted_whites = cleanup_card_list(wanted_whites)
+
+	var selection SelectedCards
+	for _, id := range wanted_blacks {
+		selection.black = append(selection.black, database.Black[id])
+	}
+	for _, id := range wanted_whites {
+		selection.white = append(selection.black, database.White[id])
+	}
+
+	return selection, nil
 }
--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"embed"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/delcaran/cah/db"
 )
@@ -21,7 +23,7 @@ func index_handler(w http.ResponseWriter, r *http.Request) {
     }
 }
 
-func db_load(w http.ResponseWriter, r *http.Request) {
+func new_game_handler(w http.ResponseWriter, r *http.Request) {
     database, _ := db.Load("eng")
 	err := templates.ExecuteTemplate(w, "new.html", database)
 	if err != nil {
@@ -29,8 +31,35 @@ func db_load(w http.ResponseWriter, r *http.Request) {
     }
 }
 
+func card_selection_handler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "Post from website! r.PostFrom = %v\n", r.PostForm)
+	//player_name := r.FormValue("name") // only one element
+    selected_sets_str := r.Form["sets"] // array of strings
+	var selected_sets []int
+	for _, s := range selected_sets_str {
+		v, err := strconv.Atoi(s)
+		if err != nil {
+			log.Fatal(err)
+		}
+		selected_sets = append(selected_sets, v)
+	}
+	loaded_cards, err := db.GetSelectedCards(selected_sets)
+	if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
+	err = templates.ExecuteTemplate(w, "test.html", loaded_cards)
+	if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
+}
+
 func main() {
     http.HandleFunc("/", index_handler)
-	http.HandleFunc("/new", db_load)
+	http.HandleFunc("/new/", new_game_handler)
+	http.HandleFunc("/select_sets/", card_selection_handler)
     log.Fatal(http.ListenAndServe(":8080", nil))
 }
